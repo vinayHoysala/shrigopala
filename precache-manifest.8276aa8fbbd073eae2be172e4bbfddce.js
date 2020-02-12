3cf0c9ed390e1e433fff9eadbf31fef28697872e
Mera Talent <meratalent@Meras-MacBook-Pro.local>
2020-02-12 20:46:29 +0530

    Updates

---
+++ precache-manifest.8276aa8fbbd073eae2be172e4bbfddce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "242a5c7d07b6a4d0059f4b70e72e0010",
+    "url": "/shrigopala/index.html"
+  },
+  {
+    "revision": "7b2d64b7919cba632878",
+    "url": "/shrigopala/static/css/main.40d59560.chunk.css"
+  },
+  {
+    "revision": "e80623c3cc68f391338b",
+    "url": "/shrigopala/static/js/2.964a0902.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/shrigopala/static/js/2.964a0902.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7b2d64b7919cba632878",
+    "url": "/shrigopala/static/js/main.b443f96c.chunk.js"
+  },
+  {
+    "revision": "7ff6e61e53909cee978c",
+    "url": "/shrigopala/static/js/runtime-main.2e29aa6c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shrigopala/static/media/logo.5d5d9eef.svg"
+  }
+]);
